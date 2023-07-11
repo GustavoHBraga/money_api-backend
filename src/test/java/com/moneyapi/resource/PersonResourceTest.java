5b25b3ae5ca764a4838ee39ea6c864729492883f
GustavoHBraga <gh.brga@gmail.com>
2023-07-10 23:38:40 -0300

    Add new test in personResource

--- src/test/java/com/moneyapi/resource/PersonResourceTest.java
+++ src/test/java/com/moneyapi/resource/PersonResourceTest.java
@@ -88,6 +88,16 @@ public class PersonResourceTest {
 	}
 	
 	@Test
+	public void testListAllEmpty() {
+		
+		when(personRepository.findAll()).thenReturn(new ArrayList<>());
+		ResponseEntity<?> response = personResource.listAll();
+		verify(personRepository, times(1)).findAll();
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+				
+	}
+	
+	@Test
 	public void testFindBycod() {
 		Person person_1 = new Person();
 		person_1.setCod(1L);
@@ -113,6 +123,31 @@ public class PersonResourceTest {
 	}
 	
 	@Test
+	public void testFindBycodNofound() {
+		Person person_1 = new Person();
+		person_1.setCod(1L);
+		person_1.setName("Gustavo");
+		person_1.setActive(true);
+		
+		InformationAdress address_1 = new InformationAdress();
+		address_1.setAddress("Rua rio paranapanema");
+		address_1.setCep("06220250");
+		address_1.setCity("OSASCO");
+		address_1.setState("Sao Paulo");
+		
+		person_1.setInformationAdress(address_1);
+		
+	
+		when(personRepository.findById(1L)).thenReturn(Optional.of(person_1));
+		
+		ResponseEntity<?> response = personResource.findOne(2L);
+		
+		verify(personRepository,times(1)).findById(2L);
+		
+		assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+	}
+	
+	@Test
 	public void testCreate() {
 		Person person_1 = new Person();
 		person_1.setCod(1L);
@@ -133,6 +168,125 @@ public class PersonResourceTest {
 		
 		assertEquals(HttpStatus.CREATED,response.getStatusCode());
 		assertEquals(person_1,response.getBody());
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		Person person_1 = new Person();
+		person_1.setCod(1L);
+		person_1.setName("Gustavo");
+		person_1.setActive(true);
+		
+		InformationAdress address_1 = new InformationAdress();
+		address_1.setAddress("Rua rio paranapanema");
+		address_1.setCep("06220250");
+		address_1.setCity("OSASCO");
+		address_1.setState("Sao Paulo");
+		
+		person_1.setInformationAdress(address_1);
+		
+		Person person_2 = new Person();
+		person_2.setCod(1L);
+		person_2.setName("Gustavo");
+		person_2.setActive(true);
+		
+		InformationAdress address_2 = new InformationAdress();
+		address_2.setAddress("Rua rio paranapanema");
+		address_2.setCep("06220250");
+		address_2.setCity("OSASCO");
+		address_2.setState("SANTOS");
+		
+		person_2.setInformationAdress(address_2);
+		
+		
+		when(personService.update(person_1.getCod(),person_2)).thenReturn(person_2);
+		
+		ResponseEntity<?> response = personResource.update(person_1.getCod(),person_2);
+		
+		verify(personService,times(1)).update(person_1.getCod(),person_2);
+		
+		assertEquals(HttpStatus.OK,response.getStatusCode());
+		assertEquals(person_2,response.getBody());
+		
+	}
+	
+	@Test
+	public void testUpdateActive() {
+		Person person_1 = new Person();
+		person_1.setCod(1L);
+		person_1.setName("Gustavo");
+		person_1.setActive(true); // -- Old
+		
+		InformationAdress address_1 = new InformationAdress();
+		address_1.setAddress("Rua rio paranapanema");
+		address_1.setCep("06220250");
+		address_1.setCity("OSASCO");
+		address_1.setState("Sao Paulo");
+		
+		person_1.setInformationAdress(address_1);
+		
+		Person person_2 = new Person();
+		person_2.setCod(1L);
+		person_2.setName("Gustavo");
+		person_2.setActive(false); // -- New 
+		
+		InformationAdress address_2 = new InformationAdress();
+		address_2.setAddress("Rua rio paranapanema");
+		address_2.setCep("06220250");
+		address_2.setCity("OSASCO");
+		address_2.setState("SANTOS");
+		
+		person_2.setInformationAdress(address_2);
+		
+		
+		when(personService.updateActive(person_1.getCod(),false)).thenReturn(person_2);
+		
+		ResponseEntity<?> response = personResource.updateActive(person_1.getCod(),false);
+		
+		verify(personService,times(1)).updateActive(person_1.getCod(), false);
+		
+		assertEquals(HttpStatus.OK,response.getStatusCode());
+		assertEquals(person_2,response.getBody());
+		
+	}
+	@Test
+	public void testUpdateAddress() {
+		Person person_1 = new Person();
+		person_1.setCod(1L);
+		person_1.setName("Gustavo");
+		person_1.setActive(true); // -- Old
+		
+		InformationAdress address_1 = new InformationAdress();
+		address_1.setAddress("Rua rio paranapanema");
+		address_1.setCep("06220250");
+		address_1.setCity("OSASCO");
+		address_1.setState("Sao Paulo");
+		
+		person_1.setInformationAdress(address_1);
+		
+		Person person_2 = new Person();
+		person_2.setCod(1L);
+		person_2.setName("Gustavo");
+		person_2.setActive(true); // -- New 
+		
+		InformationAdress address_2 = new InformationAdress();
+		address_2.setAddress("Bahia dos alvaros");
+		address_2.setCep("06220250");
+		address_2.setCity("Piaui");
+		address_2.setState("Piaui");
+		
+		person_2.setInformationAdress(address_2);
+		
+		
+		when(personService.updateAdress(person_1.getCod(),address_2)).thenReturn(person_2);
+		
+		ResponseEntity<?> response = personResource.updateAdress(person_1.getCod(),address_2);
+		
+		verify(personService,times(1)).updateAdress(person_1.getCod(), address_2);
+		
+		assertEquals(HttpStatus.OK,response.getStatusCode());
+		assertEquals(person_2,response.getBody());
 		
 	}
 	
